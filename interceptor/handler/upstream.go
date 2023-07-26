592775181219c7546efa46b70ac21165210e00f1
Jia-Huei Chen <tommy351@gmail.com>
2023-07-26 11:52:25 +0800

    Do not override request host in interceptor

--- interceptor/handler/upstream.go
+++ interceptor/handler/upstream.go
@@ -40,12 +40,11 @@ func (uh *Upstream) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	proxy.Transport = uh.roundTripper
 	proxy.Director = func(req *http.Request) {
 		req.URL = stream
-		req.Host = stream.Host
 		req.URL.Path = r.URL.Path
 		req.URL.RawQuery = r.URL.RawQuery
 		// delete the incoming X-Forwarded-For header so the proxy
 		// puts its own in. This is also important to prevent IP spoofing
-		req.Header.Del("X-Forwarded-For ")
+		req.Header.Del("X-Forwarded-For")
 	}
 	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
 		sh := NewStatic(http.StatusBadGateway, err)
